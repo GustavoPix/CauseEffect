d456ab48e52a0cc3b8f71cc60e276e96ef2f47f6
Gustavo Carvalho <gustavo1net@hotmail.com>
2022-02-10 21:54:26 -0300

    test data

---
+++ main.js
@@ -0,0 +1,19 @@
+(function CauseEffect() {
+	const people = [
+		{ name: "Kevin Casanova", street: "Rua Miranda Bezerra, 801", city: "Matias do Sul", state: "...", country: "Brasil", telephone: "(72) 8746-5000", birthday: "1971-12-24" },
+		{ name: "Ana Meireles", street: "Av. Franco, 2", city: "Porto Franco", state: "...", country: "Brasil", telephone: "(32) 2874-1183", birthday: "1974-10-22" },
+		{ name: "Martinho Serna", street: "R. Micaela Barreto, 04593. Apto 9413", city: "Quintana do Sul", state: "...", country: "Brasil", telephone: "(19) 2132-5721", birthday: "1985-04-08" },
+		{ name: "Sophie Beltrão", street: "Rua Samanta Pacheco, 54110", city: "Porto Silvana do Leste", state: "...", country: "Brasil", telephone: "(93) 97683-8467", birthday: "1964-02-12" },
+		{ name: "Antônio Galindo", street: "R. Fernando Santana, 1356. Fundos", city: "Vicente d'Oeste", state: "...", country: "Brasil", telephone: "(76) 7419-2931", birthday: "1977-04-18" },
+		{ name: "Suzana Aragão", street: "Av. Carla Jimenes, 75", city: "Camila d'Oeste", state: "...", country: "Brasil", telephone: "(81) 98358-9467", birthday: "1994-02-05" },
+		{ name: "Antônio Pacheco", street: "R. Fábio, 72. Apto 3", city: "Emiliano do Sul", state: "...", country: "Brasil", telephone: "(95) 2962-6714", birthday: "1987-12-10" },
+		{ name: "Inácio Flores", street: "Avenida Sandoval, 4081", city: "Fontes do Sul", state: "...", country: "Brasil", telephone: "(78) 3386-3096", birthday: "1987-06-27" },
+		{ name: "Maximiano Quintana", street: "Rua Ian Martines, 8983", city: "Godói do Leste", state: "...", country: "Brasil", telephone: "(71) 97797-3147", birthday: "1975-01-19" },
+		{ name: "Michele Delvalle", street: "Rua Bruno, 98724", city: "Neves do Leste", state: "...", country: "Brasil", telephone: "(67) 8312-9673", birthday: "1980-07-15" },
+		{ name: "Michele Abreu", street: "Rua Manuela Vieira, 7. Bloco A", city: "Perez d'Oeste", state: "...", country: "Brasil", telephone: "(68) 7712-7933", birthday: "2001-04-07" },
+		{ name: "Daniel Beltrão", street: "R. Emiliano Quintana, 1299", city: "Elizabeth do Norte", state: "...", country: "Brasil", telephone: "(62) 2177-6837", birthday: "1964-04-07" },
+		{ name: "Dante Burgos", street: "R. Gabriel, 45", city: "Serrano do Leste", state: "...", country: "Brasil", telephone: "(41) 3734-4083", birthday: "1987-12-21" },
+		{ name: "Adriano de Oliveira", street: "Largo Manuel, 119. Fundos", city: "Vila Irene do Norte", state: "...", country: "Brasil", telephone: "(87) 2616-4552", birthday: "1989-07-17" },
+		{ name: "Ivana Vale", street: "Av. Tamoio, 3. Bloco B", city: "São Luna", state: "...", country: "Brasil", telephone: "(24) 98708-5643", birthday: "1967-09-07" }
+	]
+})();
